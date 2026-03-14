d5090f732cced3baac06abe8f083a42a9757a7f1
Spencer Torres <contact@spencertorres.com>
2026-03-13 18:36:42 -0700

    add debug program for outputting curve results

---
+++ cmd/plot-timerange/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"hash/crc64"
+	"math/rand/v2"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"otelspam/internal/user"
+)
+
+const (
+	runs = 40000000
+)
+
+// curve config
+var cfg = user.TimeRangeConfig{
+	Type:  user.TimeRangeLogNormal,
+	Round: 1 * time.Minute,
+	Mean:  26 * time.Hour,
+	Min:   60 * time.Minute,
+	Max:   48 * time.Hour,
+	Sigma: 0.2,
+}
+
+func newRand(seed string, workerID int) *rand.Rand {
+	crc := crc64.Checksum([]byte(seed), crc64.MakeTable(crc64.ISO))
+	return rand.New(rand.NewPCG(crc, uint64(workerID)))
+}
+
+func main() {
+	rng := newRand("hyperdx", 0)
+	buckets := map[time.Duration]int{}
+
+	for range runs {
+		d := cfg.SampleLookback(rng)
+		d = d.Truncate(cfg.Round)
+		buckets[d]++
+	}
+
+	keys := make([]time.Duration, 0, len(buckets))
+	for k := range buckets {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
+
+	w := csv.NewWriter(os.Stdout)
+	w.Write([]string{"x_minutes", "y_count"})
+	for _, k := range keys {
+		pct := float64(buckets[k]) / float64(runs) * 100
+		w.Write([]string{
+			strconv.FormatFloat(k.Minutes(), 'f', 4, 64),
+			// strconv.Itoa(buckets[k]),
+			strconv.FormatFloat(pct, 'f', 3, 64),
+		})
+	}
+	w.Flush()
+
+	if err := w.Error(); err != nil {
+		fmt.Fprintf(os.Stderr, "csv write error: %v\n", err)
+		os.Exit(1)
+	}
+}
